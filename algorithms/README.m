3de4cdc945fe621d4cb8aa1e2a58ab5b8a2a9d43
Harshit <52852877+ahv15@users.noreply.github.com>
2025-05-30 21:42:58 -0400

    Create algorithms directory structure

---
+++ algorithms/README.m
@@ -0,0 +1,8 @@
+% ALGORITHMS DIRECTORY
+% This directory contains machine learning algorithm implementations
+%
+% Subdirectories:
+% - optimization/    - Gradient descent variants (Adam, RMSProp, etc.)
+% - regression/      - Linear and logistic regression implementations  
+% - clustering/      - K-means, Fuzzy C-means clustering algorithms
+% - classification/  - Naive Bayes and other classification methods
